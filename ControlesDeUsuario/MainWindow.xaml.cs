6d696d199959222dfbf7faaf27fbc0f908fdc053
morapx <bryan_alexis_1999@hotmail.com>
2019-09-26 12:58:11 -0700

    nuevo

--- ControlesDeUsuario/MainWindow.xaml.cs
+++ ControlesDeUsuario/MainWindow.xaml.cs
@@ -52,5 +52,56 @@ namespace ControlesDeUsuario
                     break;
             }
         }
+
+        private void BotonCalcularArea_Click(object sender, RoutedEventArgs e)
+        {
+            double area = 0.0;
+
+            switch (ComboFifura.SelectedIndex)
+            {
+                case 0: //circulo
+                    double radio = double.Parse(((ParametrosCirculo)(GrindParametrosFigura.Children[0])).TextoRadio.Text);
+                    area = Math.PI * radio * radio;
+                        break;
+
+
+
+                case 1: //triangulo
+                    double baasetriangulo = double.Parse(((ParametrosTriangulo)(GrindParametrosFigura.Children[0])).TextoBaseTriangulo.Text);
+                    double alturatriangulo = double.Parse(((ParametrosTriangulo)(GrindParametrosFigura.Children[0])).TextoBaseTriangulo.Text);
+                    area = baasetriangulo * alturatriangulo / 2;
+                    break;
+
+                case 2: //Rectangulo
+                    double baaserectangulo = double.Parse(((ParametrosRectangulo)(GrindParametrosFigura.Children[0])).TextoBaseRectangulo.Text);
+                    double alturarectangulo = double.Parse(((ParametrosRectangulo)(GrindParametrosFigura.Children[0])).TextoAlturaRectangulo.Text);
+                    area = baaserectangulo * alturarectangulo / 2;
+                    break;
+
+                case 3: //Pentagono
+                    double perimetropentagono = double.Parse(((ParametroPentagono)(GrindParametrosFigura.Children[0])).TextoPerimetroPentagono.Text);
+                    double apotemapentagono = double.Parse(((ParametroPentagono)(GrindParametrosFigura.Children[0])).TextoApotemaPentagnono.Text);
+                    area = perimetropentagono * apotemapentagono / 2;
+                    break;
+
+                case 4: //Cuadrado
+                    double ladocuadrado = double.Parse(((ParametroCuadrado)(GrindParametrosFigura.Children[0])).TextoLadoCuadrado.Text);
+
+                    area = ladocuadrado * ladocuadrado;
+                    break;
+
+                case 5: //Rectangulo
+                    double basemenor = double.Parse(((ParametroTrapecio)(GrindParametrosFigura.Children[0])).Textobasemenortrapecio.Text);
+                    double basemayor = double.Parse(((ParametroTrapecio)(GrindParametrosFigura.Children[0])).TextoBaseMayorTrapecio.Text);
+                    double alturatrapecio = double.Parse(((ParametroTrapecio)(GrindParametrosFigura.Children[0])).TextoAlturaTrapeco.Text);
+                    area = ((basemenor + basemayor) * alturatrapecio) / 2;
+                    break;
+
+
+                default:
+                    break;
+            }
+            LabelResultado.Text = area.ToString();
+        }
     }
 }
